866cf4c19b731b6d05f8cfc531e2407c9e271b03
MRFOVE\Fove <fovehack@gmail.com>
2023-06-14 17:30:34 +0200

    first commit

---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+export function Navigation() {
+  return (
+    <div className="flex justify-between py-3">
+      <Link to="/tasks">
+        <h1 className="font-bold text-3x1 mb-4">Task App</h1>
+      </Link>
+      <button className="bg-indigo-500 px-3 py-2 rounded-lg">
+        <Link to="/tasks-create">Create task</Link>
+      </button>
+    </div>
+  );
+}
